0c090352a9496fa909a5dc5fb69eeaee8ce3e187
Caspian <zhaodonghao586@outlook.com>
2023-04-12 15:29:12 +0800

    feat: support to i18n.

---
+++ src/i18n/en.ts
@@ -0,0 +1,10 @@
+import zh_cn from "./zh-cn";
+
+const en: typeof zh_cn = {
+    article_list: "Article List",
+    latest: "Latest",
+    popular: "Popular",
+    follow: "Follow",
+}
+
+export default en
---
+++ src/i18n/zh-cn.ts
@@ -0,0 +1,8 @@
+const zh_cn = {
+    article_list: '文章列表',
+    latest: '最新',
+    popular: '最热',
+    follow: '关注',
+}
+
+export default zh_cn
