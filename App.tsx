8e87df47c8315e5810d5d699d0fbdf3ad86b8575
Alan Toa <37520667+alantoa@users.noreply.github.com>
2023-04-17 00:07:44 +0800

    pref: add react-native-screens (#18)

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
-import './shim'
-import 'react-native-gesture-handler';
-import 'react-native-url-polyfill/auto';
+import "./shim";
+import "react-native-gesture-handler";
+import "react-native-url-polyfill/auto";
 
-import { useFonts } from 'expo-font'
-import { useColorScheme } from 'react-native'
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import { TamaguiProvider, Theme } from 'tamagui'
-import config from './tamagui.config'
-import { RootNavigator } from './src/navigation';
-import { useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createClient, WagmiConfig } from "wagmi"
-import { getDefaultClientConfig } from '@/lib/get-default-client-config';
-
-SplashScreen.preventAutoHideAsync()
+import { useFonts } from "expo-font";
+import { useColorScheme } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { TamaguiProvider, Theme } from "tamagui";
+import config from "./tamagui.config";
+import { RootNavigator } from "./src/navigation";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createClient, WagmiConfig } from "wagmi";
+import { getDefaultClientConfig } from "@/lib/get-default-client-config";
+import { enableScreens, enableFreeze } from "react-native-screens";
+enableScreens(true);
+enableFreeze(true);
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,30 +27,30 @@ const queryClient = new QueryClient({
       cacheTime: 1000 * 60 * 60 * 24, // 24 hours
     },
   },
-})
+});
 
-const wagmiClient = createClient(getDefaultClientConfig({ appName: 'xLog' }))
+const wagmiClient = createClient(getDefaultClientConfig({ appName: "xLog" }));
 
 export default () => {
-  const colorScheme = useColorScheme()
+  const colorScheme = useColorScheme();
 
   const [loaded] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
-  })
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
 
   useEffect(() => {
-    loaded && SplashScreen.hideAsync()
-  }, [loaded])
+    loaded && SplashScreen.hideAsync();
+  }, [loaded]);
 
   if (!loaded) {
-    return null
+    return null;
   }
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <TamaguiProvider config={config}>
-        <Theme name={colorScheme === 'dark' ? 'dark' : 'light'}>
+        <Theme name={colorScheme === "dark" ? "dark" : "light"}>
           <NavigationContainer>
             <SafeAreaProvider>
               <WagmiConfig client={wagmiClient}>
@@ -62,7 +64,7 @@ export default () => {
       </TamaguiProvider>
     </GestureHandlerRootView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
