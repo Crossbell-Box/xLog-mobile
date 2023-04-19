ab297905a17d8b21af22f6dc85c38df1ebe63a6d
Caspian <zhaodonghao586@outlook.com>
2023-04-20 00:50:33 +0800

    chore: remove hot-update test.

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -2,7 +2,7 @@ import zh_cn from "./zh-cn";
 
 const en: typeof zh_cn = {
     feed: "Feed",
-    latest: "Latest1",
+    latest: "Latest",
     hot: "Hot",
     following: "Following",
     ago: "{{time}} ago",
