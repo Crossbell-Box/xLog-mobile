ca7f34700b3a3388a4f64354dc3f5de6db8bb076
Steve Yu <steveyuowo@gmail.com>
2023-05-08 01:57:00 +0800

    fix: load .env.undefined when NODE_ENV empty

--- app.config.ts
+++ app.config.ts
@@ -3,8 +3,11 @@ import type { ExpoConfig, ConfigContext } from "expo/config";
 
 import { version } from "./package.json";
 
+dotenv.config({ path: ".env" });
 dotenv.config({ path: ".env.common" });
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+if (process.env.NODE_ENV) {
+  dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+}
 
 export default (_: ConfigContext): ExpoConfig => {
   return {
