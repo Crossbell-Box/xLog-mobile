1ed74fbb1cf194b523b5fdf35184c76cd5a54b4e
Caspian <zhaodonghao586@outlook.com>
2023-05-08 17:04:40 +0800

    feat: comments page.

--- src/components/ProfilePageHeader/index.tsx
+++ src/components/ProfilePageHeader/index.tsx
@@ -11,7 +11,7 @@ export const ProfilePageHeader: React.FC<ProfilePageHeaderProps> = (props) => {
   const { title, description } = props;
   return (
     <YStack marginBottom="$6">
-      <H3 marginBottom="$3">{title}</H3>
+      <H3 marginBottom={description ? "$3" : "$-3"}>{title}</H3>
       {description && <SizableText size={"$sm"} color={"$colorSubtitle"}>{description}</SizableText>}
     </YStack>
   );
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,12 +3,18 @@ import { initReactI18next } from "react-i18next";
 import * as Localization from "expo-localization";
 import i18next from "i18next";
 
-import en from "./en.json";
-import zh from "./zh.json";
+import en from "./en/index.json";
+import zh_common from "./zh/common.json";
+import zh from "./zh/index.json";
 
 const resources = {
-  zh: { translation: zh },
-  en: { translation: en },
+  zh: {
+    translation: zh,
+    common: zh_common,
+  },
+  en: {
+    translation: en,
+  },
 };
 
 i18next
