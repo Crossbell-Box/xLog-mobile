6d5206c867154686502eacbb75cebae92284fe26
Caspian <zhaodonghao586@outlook.com>
2023-05-11 17:04:18 +0800

    feat: notification page completed.

--- src/constants/resource.ts
+++ src/constants/resource.ts
@@ -1,3 +1,4 @@
 export { default as LogoDark } from "../assets/logo-dark.svg";
 export { default as LogoLight } from "../assets/logo-light.svg";
+export { default as CrossbellLogo } from "../assets/crossbell-logo.svg";
 
