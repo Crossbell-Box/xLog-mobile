c951625ff273cab85d354ec97d8eafa63fd36dca
Caspian <zhaodonghao586@outlook.com>
2023-06-24 14:07:45 +0800

    chore: inject env correctly.

--- scripts/set-app-config-env.js
+++ scripts/set-app-config-env.js
@@ -2,56 +2,60 @@ const dotenv = require("dotenv");
 
 const { version } = require("../package.json");
 
-const ENV = process.env.NODE_ENV ?? "production";
+function setAppConfigEnv() {
+  const ENV = process.env.NODE_ENV ?? "production";
 
-if (process.env.EAS_BUILD === "true") {
-  dotenv.config({ path: process.env.ENV_FILE_COMMON });
-  dotenv.config({ path: process.env[`ENV_FILE_${ENV.toUpperCase()}`] });
-}
-else {
-  dotenv.config({ path: ".env.common" });
-  dotenv.config({ path: `.env.${ENV}` });
-}
+  if (process.env.EAS_BUILD === "true") {
+    dotenv.config({ path: process.env.ENV_FILE_COMMON });
+    dotenv.config({ path: process.env[`ENV_FILE_${ENV.toUpperCase()}`] });
+  }
+  else {
+    dotenv.config({ path: ".env.common" });
+    dotenv.config({ path: `.env.${ENV}` });
+  }
 
-console.log("ENV:", ENV);
+  console.log("ENV:", ENV);
 
-const SCHEME = process.env.APP_SCHEME;
-const HOST = process.env.APP_HOST;
+  const SCHEME = process.env.APP_SCHEME;
+  const HOST = process.env.APP_HOST;
 
-const envConfig = {
-  development: {
-    name: "xLog-dev",
-    host: HOST,
-    scheme: `${SCHEME}.development`,
-    icon: "./assets/icon.development.png",
-  },
-  staging: {
-    name: "xLog-preview",
-    host: HOST,
-    scheme: `${SCHEME}.staging`,
-    icon: "./assets/icon.staging.png",
-  },
-  production: {
-    name: "xLog",
-    host: HOST,
-    scheme: SCHEME,
-    icon: "./assets/icon.png",
-  },
-};
+  const envConfig = {
+    development: {
+      name: "xLog-dev",
+      host: HOST,
+      scheme: `${SCHEME}.development`,
+      icon: "./assets/icon.development.png",
+    },
+    staging: {
+      name: "xLog-preview",
+      host: HOST,
+      scheme: `${SCHEME}.staging`,
+      icon: "./assets/icon.staging.png",
+    },
+    production: {
+      name: "xLog",
+      host: HOST,
+      scheme: SCHEME,
+      icon: "./assets/icon.png",
+    },
+  };
 
-/**
+  /**
  * Ignoring patch version
  * @example 1.1.1 -> 1.1.0 / 4.2.1 -> 4.2.0
  * */
-function decrementVersion(version) {
-  const parts = version.split(".");
-  parts[parts.length - 1] = "0";
-  return parts.join(".");
+  function decrementVersion(version) {
+    const parts = version.split(".");
+    parts[parts.length - 1] = "0";
+    return parts.join(".");
+  }
+
+  return {
+    version,
+    appConfig: envConfig[ENV],
+    decreasedVersion: decrementVersion(version),
+    environment: ENV,
+  };
 }
 
-module.exports = {
-  version,
-  appConfig: envConfig[ENV],
-  decreasedVersion: decrementVersion(version),
-  environment: ENV,
-};
+module.exports = setAppConfigEnv;
