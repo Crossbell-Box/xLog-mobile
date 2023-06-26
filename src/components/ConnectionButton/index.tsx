4e0347c2913400361f9ab54a7f9a360169a40986
Caspian <zhaodonghao586@outlook.com>
2023-06-26 22:01:03 +0800

    fix: Fixed compatibility issues with wc2

--- src/components/ConnectionButton/index.tsx
+++ src/components/ConnectionButton/index.tsx
@@ -45,7 +45,18 @@ const providerMetadata: IProviderMetadata = {
 
 const sessionParams: ConnectParams = {
   namespaces: {
-    // ...
+    eip155: {
+      methods: [
+        "eth_sendTransaction",
+        "eth_signTransaction",
+        "eth_sign",
+        "personal_sign",
+        "eth_signTypedData",
+      ],
+      chains: ["eip155:1"],
+      events: ["chainChanged", "accountsChanged"],
+      rpcMap: {},
+    },
   },
 };
 
