f028740df00491b74ffb88b0e3133973ef078306
Caspian <zhaodonghao586@outlook.com>
2023-07-04 11:27:10 +0800

    fix: Modified the condition of the IS_CI flag

--- scripts/set-app-config-env.js
+++ scripts/set-app-config-env.js
@@ -4,9 +4,9 @@ const { version } = require("../package.json");
 
 function setAppConfigEnv() {
   const ENV = process.env.NODE_ENV ?? "production";
-  const IS_EAS_CI = process.env.EAS_BUILD === "true";
+  const IS_CI = process.env.EAS_BUILD === "true" || process.env.GITHUB_ACTIONS === "true";
 
-  if (IS_EAS_CI) {
+  if (IS_CI) {
     dotenv.config({ path: process.env.ENV_FILE_COMMON });
     dotenv.config({ path: process.env[`ENV_FILE_${ENV.toUpperCase()}`] });
   }
@@ -26,24 +26,24 @@ function setAppConfigEnv() {
       host: HOST,
       scheme: `${SCHEME}.development`,
       icon: "./assets/icon.development.png",
-      androidGoogleServicesFile: IS_EAS_CI ? process.env.ANDROID_GOOGLE_SERVICES_DEVELOPMENT : "./google-services.development.json",
-      iosGoogleServicesFile: IS_EAS_CI ? process.env.IOS_GOOGLE_SERVICES_DEVELOPMENT : "./GoogleService-Info.development.plist",
+      androidGoogleServicesFile: IS_CI ? process.env.ANDROID_GOOGLE_SERVICES_DEVELOPMENT : "./google-services.development.json",
+      iosGoogleServicesFile: IS_CI ? process.env.IOS_GOOGLE_SERVICES_DEVELOPMENT : "./GoogleService-Info.development.plist",
     },
     staging: {
       name: "xLog-preview",
       host: HOST,
       scheme: `${SCHEME}.staging`,
       icon: "./assets/icon.staging.png",
-      androidGoogleServicesFile: IS_EAS_CI ? process.env.ANDROID_GOOGLE_SERVICES_STAGING : "./google-services.staging.json",
-      iosGoogleServicesFile: IS_EAS_CI ? process.env.IOS_GOOGLE_SERVICES_STAGING : "./GoogleService-Info.staging.plist",
+      androidGoogleServicesFile: IS_CI ? process.env.ANDROID_GOOGLE_SERVICES_STAGING : "./google-services.staging.json",
+      iosGoogleServicesFile: IS_CI ? process.env.IOS_GOOGLE_SERVICES_STAGING : "./GoogleService-Info.staging.plist",
     },
     production: {
       name: "xLog",
       host: HOST,
       scheme: SCHEME,
       icon: "./assets/icon.png",
-      androidGoogleServicesFile: IS_EAS_CI ? process.env.ANDROID_GOOGLE_SERVICES_PRODUCTION : "./google-services.production.json",
-      iosGoogleServicesFile: IS_EAS_CI ? process.env.IOS_GOOGLE_SERVICES_PRODUCTION : "./GoogleService-Info.production.plist",
+      androidGoogleServicesFile: IS_CI ? process.env.ANDROID_GOOGLE_SERVICES_PRODUCTION : "./google-services.production.json",
+      iosGoogleServicesFile: IS_CI ? process.env.IOS_GOOGLE_SERVICES_PRODUCTION : "./GoogleService-Info.production.plist",
     },
   };
 
