2205f4e6581b327769199467dcef96a6cf09c03c
Caspian <zhaodonghao586@outlook.com>
2023-11-21 00:31:42 +0000

    feat: Update IPFS gateway URL

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -4,7 +4,7 @@ import Constants from "expo-constants";
 export const INFURA_ID = Constants.expoConfig.extra?.INFURA_ID;
 export const SENTRY_DSN = Constants.expoConfig.extra?.SENTRY_DSN;
 export const WALLET_PROJECT_ID = Constants.expoConfig.extra?.WALLET_PROJECT_ID;
-export const IPFS_GATEWAY = "https://ipfs.xlog.app/ipfs/";
+export const IPFS_GATEWAY = "https://ipfs.crossbell.io/ipfs/";
 export const WALLET_RELAY_URL = "wss://relay.walletconnect.com";
 export const CSB_SCAN = Constants.expoConfig.extra?.CSB_SCAN || "https://scan.crossbell.io";
 export const CSB_XCHAR = Constants.expoConfig.extra?.CSB_XCHAR || "https://xchar.app";
